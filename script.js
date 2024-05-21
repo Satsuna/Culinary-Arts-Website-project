3acc311d0155ea7cb19557c0776193939d04dd80
Satsuna <jvbade@gmail.com>
2024-05-21 19:02:58 +0800

    dishes done

---
+++ script.js
@@ -0,0 +1,7 @@
+function sideDishOnClick() {
+    document.getElementById("logoID").src = "CSS/resources/9.png";
+}
+
+function dessertOnClick() {
+    document.getElementById("logoID").src = "CSS/resources/10.png";
+}
