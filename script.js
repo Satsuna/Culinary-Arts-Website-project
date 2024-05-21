fca5d790b3e3b80aa161bd21e72383e02652d60b
Satsuna <jvbade@gmail.com>
2024-05-21 19:12:17 +0800

    placeholders lang

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 function sideDishOnClick() {
     document.getElementById("logoID").src = "CSS/resources/9.png";
+    document.getElementById("pic1").src = "CSS/resources/placeholder2.jpg"
+    document.getElementById("pic2").src = "CSS/resources/placeholder2.jpg"
+    document.getElementById("pic3").src = "CSS/resources/placeholder2.jpg"
 }
 
 function dessertOnClick() {
     document.getElementById("logoID").src = "CSS/resources/10.png";
+    document.getElementById("pic1").src = "CSS/resources/placeholder.png"
+    document.getElementById("pic2").src = "CSS/resources/placeholder.png"
+    document.getElementById("pic3").src = "CSS/resources/placeholder.png"
 }
