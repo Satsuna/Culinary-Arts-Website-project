c6ea9cae8b8f1a1891e7fd41b39a3e6cf9e9eba5
Satsuna <jvbade@gmail.com>
2024-05-23 23:38:42 +0800

    pictures for dishes

--- sites/script.js
+++ sites/script.js
@@ -1,13 +1,13 @@
 function sideDishOnClick() {
     document.getElementById("logoID").src = "CSS/resources/9.png";
-    document.getElementById("pic1").src = "CSS/resources/placeholder2.jpg"
-    document.getElementById("pic2").src = "CSS/resources/placeholder2.jpg"
-    document.getElementById("pic3").src = "CSS/resources/placeholder2.jpg"
+    document.getElementById("pic1").src = "CSS/resources/Sdish1.jpg"
+    document.getElementById("pic2").src = "CSS/resources/Sdish2.jpg"
+    document.getElementById("pic3").src = "CSS/resources/Sdish3.jpg"
 }
 
 function dessertOnClick() {
     document.getElementById("logoID").src = "CSS/resources/10.png";
-    document.getElementById("pic1").src = "CSS/resources/placeholder.png"
-    document.getElementById("pic2").src = "CSS/resources/placeholder.png"
-    document.getElementById("pic3").src = "CSS/resources/placeholder.png"
+    document.getElementById("pic1").src = "CSS/resources/Dessert1.jpg"
+    document.getElementById("pic2").src = "CSS/resources/Dessert2.jpg"
+    document.getElementById("pic3").src = "CSS/resources/Dessert3.jpg"
 }
